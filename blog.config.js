1e3b49af72fe9130ba840c0d55698df4e69f6f98
yuntech-git <weiyh@yunj.com.cn>
2023-07-18 08:30:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Vast Wei',
   email: 'weiyh@126.com',
   link: 'https://qisware.com',
-  description: 'This gonna be an awesome website.',
+  description: 'Be present.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
